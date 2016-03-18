00ba8be66e3b8f0a82d6565f85b7ee272580218e
Kent Gray <kentgray@users.noreply.github.com>
2016-03-18 13:21:24 -0500

    Added files via upload

---
+++ Team-Projects/FFCN/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FFCNMaintenance.Startup))]
+namespace FFCNMaintenance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
