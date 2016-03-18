c43eae83ee1c313e744e876ef8aff52d35aaff30
Kent Gray <kentgray@users.noreply.github.com>
2016-03-18 13:10:48 -0500

    Added files via upload

---
+++ C#/Sample C# Applications/Calculator/CalcApp/Calculator App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+
+class Calculator
+{
+
+    static void Main()
+    {
+        Console.Write("First Number: ");
+        string firstNumberInput = Console.ReadLine();
+        double firstNumber = double.Parse(firstNumberInput);
+
+        Console.Write("Second Number: ");
+        string secondNumberInput = Console.ReadLine();
+        double secondNumber = double.Parse(secondNumberInput);
+
+        double result = firstNumber + secondNumber;
+
+        Console.WriteLine($"\n\tYour result is {result}.");
+
+        Console.ReadKey();
+    }
+}
